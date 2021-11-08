c5357ccf5935213ffdbf8441558e62fceceb03d0
Dalton Otineru <d@dmbp.local>
2021-11-08 11:05:20 -0800

    guess game

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-/*
-let submitGuess = document.getElementById("submitGuess"); 
-let correctNum = Math.floor(Math.random() * 100) + 1;
-function guessGame() {
-    let result = document.getElementById("result");  
-    let number = document.getElementById("numberGuess").value;
-    
-    console.log(correctNum);
-    let submitGuess = document.getElementById("submitGuess"); 
-    if (number == correctNum){
-        //numberGuess();
-       result.innerHTML = "You guessed the correct number!";
-    } if (number < correctNum){
-       // numberGuess();
-        result.innerHTML = "Your guess was too low!";
-    }
-    if (number > correctNum){
-       // numberGuess();
-        result.innerHTML = "Your guess was too high!";
-    }
-};
-*/
 
 let submitGuess = document.getElementById("submitGuess"); 
 let correctNum = Math.floor(Math.random() * 100) + 1;
@@ -28,24 +6,20 @@ let guessCount = 1;
 function guessGame() {
     let result = document.getElementById("result");  
     let number = document.getElementById("numberGuess").value;
-    
-    console.log(correctNum);
     let submitGuess = document.getElementById("submitGuess"); 
     if (number == correctNum){
+        result.innerHTML = "You guessed the correct number!";
+     } else if(guessCount === 3){
+         gameOver.innerHTML = `You've used up all your guesses! The correct number was ${correctNum}`;
+     } if (number < correctNum){
+         result.innerHTML = "Your guess was too low!";
+     } if (number > correctNum) {
+         result.innerHTML = "Your Guess was too high!";
+     }
+     guessCount++
+ }
         
-       result.innerHTML = "You guessed the correct number!";
-    } else if(guessCount === 3){
-        gameOver.innerHTML = `You've used up all your guesses! The correct number was ${correctNum}`;
-    } if (number < correctNum){
-        result.innerHTML = "Your guess was too low!";
-    } if (number > correctNum) {
-        result.innerHTML = "Your Guess was too high!";
-    }
-    guessCount++
-}
-     
-    
-       
+
 
 
 
